d7733a99a0137c9514a30b2604ed448f1928b9ae
lw0626 <1956906867@qq.com>
2020-11-03 19:01:43 +0800

    modify

---
+++ pintos-lw/src/threads/synch.h
@@ -0,0 +1,58 @@
+#ifndef THREADS_SYNCH_H
+#define THREADS_SYNCH_H
+
+#include <list.h>
+#include <stdbool.h>
+
+/* A counting semaphore. */
+struct semaphore 
+  {
+    unsigned value;             /* Current value. */
+    struct list waiters;        /* List of waiting threads. */
+  };
+
+void sema_init (struct semaphore *, unsigned value);
+void sema_down (struct semaphore *);
+bool sema_try_down (struct semaphore *);
+void sema_up (struct semaphore *);
+void sema_self_test (void);
+bool wait_cmp_priority (const struct list_elem *a, const struct list_elem *b, void *aux);
+
+/* Lock. */
+struct lock 
+  {
+    struct thread *holder;      /* Thread holding lock (for debugging). */
+    struct semaphore semaphore; /* Binary semaphore controlling access. */
+    int max;                    /* 最大的优先级 */
+    struct list_elem elem;      /*  */
+  };
+
+void lock_init (struct lock *);
+void lock_acquire (struct lock *);
+bool lock_try_acquire (struct lock *);
+void lock_release (struct lock *);
+bool lock_held_by_current_thread (const struct lock *);
+void thread_hold_the_lock(struct lock *lock);
+void thread_update_priority (struct thread *t);
+bool lock_cmp_priority(struct list_elem *a,struct list_elem *b,void *aux);
+
+/* Condition variable. */
+struct condition 
+  {
+    struct list waiters;        /* List of waiting threads. */
+  };
+
+void cond_init (struct condition *);
+void cond_wait (struct condition *, struct lock *);
+void cond_signal (struct condition *, struct lock *);
+void cond_broadcast (struct condition *, struct lock *);
+bool cond_compare (struct list_elem *a, struct list_elem *b, void *aux);
+
+/* Optimization barrier.
+
+   The compiler will not reorder operations across an
+   optimization barrier.  See "Optimization Barriers" in the
+   reference guide for more information.*/
+#define barrier() asm volatile ("" : : : "memory")
+
+#endif /* threads/synch.h */
