c1201267d3a564754128d3049e230dd2b7c60517
Rainy-Sky <54521443+Rainy-Sky@users.noreply.github.com>
2020-11-08 21:57:41 +0800

    Revert "Update synch.h"
    This reverts commit 340d2bf26b67dc9a70ec5e6d80bd72a5b0bd0b2f.

--- pintos/src/threads/synch.h
+++ pintos/src/threads/synch.h
@@ -32,6 +32,7 @@ void lock_acquire (struct lock *);
 void update_thread(struct lock *lock);
 bool lock_try_acquire (struct lock *);
 void lock_release (struct lock *);
+bool lock_held_by_current_thread (const struct lock *);
 
 void thread_update_priority (struct thread *t);
 bool lock_cmp_priority(const struct list_elem *a,const struct list_elem *b,void *aux);
